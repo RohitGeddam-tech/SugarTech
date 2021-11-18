b96c0042b6872d9634cdb2a33d41060ab9940296
Rohi_Geddam <76106300+RohitGeddam-tech@users.noreply.github.com>
2021-11-18 20:40:13 +0530

    Add files via upload
    loader added to banner text

--- script.js
+++ script.js
@@ -40,43 +40,70 @@ var rightBoxesRef = document.getElementById("rightBoxesRef");
 var leftBoxesRef = document.getElementById("leftBoxesRef");
 var blackCerckle = document.getElementById("blackCerckle");
 
-window.addEventListener("load", () => {
-  // console.warn("left bound: ",leftDiv.getBoundingClientRect().top);
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
 
-  function myFunction3() {
+var a = true;
+var b = false;
+
+let loada = () => {
+  if (a) {
+    one.classList.add("active");
     first1.classList.add("active");
-    third3.classList.remove("active");
-    second2.classList.remove("active");
-    firstImg.src = "./image/website2.gif";
-  }
-
-  function myFunction() {
-    setTimeout(function () {
+    one.addEventListener("animationend", () => {
+      one.classList.remove("active");
+      two.classList.add("active");
+      first.classList.remove("active");
+      third.classList.remove("active");
+      second.classList.add("active");
       first1.classList.remove("active");
       third3.classList.remove("active");
       second2.classList.add("active");
       firstImg.style.display = "none";
       secondImg.style.display = "block";
       thirdImg.style.display = "none";
-    }, 5000);
-  }
-
-  function myFunction2() {
-    setTimeout(function () {
-      third3.classList.add("active");
+    });
+    two.addEventListener("animationend", () => {
+      two.classList.remove("active");
+      three.classList.add("active");
+      third.classList.add("active");
+      second.classList.remove("active");
       first1.classList.remove("active");
+      third3.classList.add("active");
       second2.classList.remove("active");
       firstImg.style.display = "none";
       secondImg.style.display = "none";
       thirdImg.style.display = "block";
-    }, 10000);
+    });
+    three.addEventListener("animationend", () => {
+      three.classList.remove("active");
+      one.classList.add("active");
+      first.classList.add("active");
+      second.classList.remove("active");
+      third.classList.remove("active");
+      first1.classList.add("active");
+      second2.classList.remove("active");
+      third3.classList.remove("active");
+      firstImg.style.display = "block";
+      secondImg.style.display = "none";
+      thirdImg.style.display = "none";
+    });
+  }
+};
+
+window.addEventListener("load", () => {
+  loada();
+
+  function myFunction3() {
+    firstImg.src = "./image/website2.gif";
   }
 
   window.addEventListener("scroll", () => {
     var scaleAsp =
       document.documentElement.scrollTop -
       here.getBoundingClientRect().top -
-      900;
+      1200;
     if (
       document.documentElement.scrollTop >
       cerckleAnimMain.getBoundingClientRect().top + 300
@@ -88,7 +115,7 @@ window.addEventListener("load", () => {
     }
     if (
       document.documentElement.scrollTop >
-      here.getBoundingClientRect().top + 900
+      here.getBoundingClientRect().top + 1200
     ) {
       blackCerckle.style.transform =
         "scale(" + scaleAsp + ", " + scaleAsp + ")";
@@ -99,18 +126,6 @@ window.addEventListener("load", () => {
 
   if (window.innerWidth > 1100) {
     window.addEventListener("scroll", () => {
-      // console.log("last img", leftDiv.getBoundingClientRect().bottom);
-      //   console.log(document.body.scrollHeight);
-      //   console.log("top", document.documentElement.scrollTop);
-      // console.log("top", window.scrollY);
-      // console.log("top", window.pageYOffset);
-
-      // if (scaleAsp > window.innerWidth + 40) {
-      //   blackCerckle.style.opacity = "0";
-      // } else {
-      //   blackCerckle.style.opacity = "1";
-      // }
-
       if (window.pageYOffset < 1140) {
         app.classList.add("black");
         app.classList.remove("white");
@@ -187,70 +202,23 @@ window.addEventListener("load", () => {
         safe2.classList.add("blackbord");
         safe2.classList.remove("whitebord");
       }
-
-      if (window.pageYOffset < 20) {
-        first.classList.add("active");
-        second.classList.remove("active");
-        third.classList.remove("active");
-        firstImg.style.display = "block";
-        secondImg.style.display = "none";
-        thirdImg.style.display = "none";
-        // console.warn("firstImg active");
-      }
-      if (window.pageYOffset > 20) {
-        first.classList.remove("active");
-        third.classList.remove("active");
-        second.classList.add("active");
-        firstImg.style.display = "none";
-        secondImg.style.display = "block";
-        thirdImg.style.display = "none";
-        // console.warn("secondImg active");
-      }
-      if (window.pageYOffset > 40) {
-        third.classList.add("active");
-        second.classList.remove("active");
-        firstImg.style.display = "none";
-        secondImg.style.display = "none";
-        thirdImg.style.display = "block";
-        // console.warn("secondImg active");
-      }
-      if (window.pageYOffset > 90) {
-        leftDiv.style.opacity = "0";
-        leftDiv.style.position = "absolute";
-      } else {
-        leftDiv.style.opacity = "1";
-        leftDiv.style.position = "fixed";
-      }
-      if (window.pageYOffset > 90) {
-        aDiv.style.opacity = "1";
-        aDiv.style.zIndex = "90";
-      } else {
-        aDiv.style.opacity = "0";
-        aDiv.style.zIndex = "70";
-      }
-      if (window.pageYOffset > 100) {
-        scrolled.style.opacity = "0";
-      } else {
-        scrolled.style.opacity = "1";
-      }
     });
   } else {
     myFunction3();
-    myFunction();
-    myFunction2();
     if (window.innerWidth > 900) {
       window.addEventListener("scroll", () => {
+        // console.log(window.pageYOffset);
         if (window.pageYOffset < 1380) {
           app.classList.add("black");
           app.classList.remove("white");
         }
 
-        if (window.pageYOffset > 1380 && window.pageYOffset < 2505) {
+        if (window.pageYOffset > 1380 && window.pageYOffset < 2475) {
           app.classList.add("white");
           app.classList.remove("black");
         }
 
-        if (window.pageYOffset > 2505 && window.pageYOffset < 6280) {
+        if (window.pageYOffset > 2475 && window.pageYOffset < 6280) {
           app.classList.add("black");
           app.classList.remove("white");
           junc.classList.add("inactive");
@@ -334,7 +302,7 @@ window.addEventListener("load", () => {
     }
     if (window.innerWidth < 900) {
       window.addEventListener("scroll", () => {
-        console.log(window.pageYOffset);
+        // console.log(window.pageYOffset);
         if (window.pageYOffset < 1280) {
           app.classList.add("black");
           app.classList.remove("white");
@@ -435,12 +403,12 @@ window.addEventListener("load", () => {
           app.classList.remove("white");
         }
 
-        if (window.pageYOffset > 1000 && window.pageYOffset < 1670) {
+        if (window.pageYOffset > 1000 && window.pageYOffset < 1870) {
           app.classList.add("white");
           app.classList.remove("black");
         }
 
-        if (window.pageYOffset > 1670 && window.pageYOffset < 6840) {
+        if (window.pageYOffset > 1870 && window.pageYOffset < 6840) {
           app.classList.add("black");
           app.classList.remove("white");
           junc.classList.add("inactive");
@@ -530,12 +498,12 @@ window.addEventListener("load", () => {
           app.classList.remove("white");
         }
 
-        if (window.pageYOffset > 1000 && window.pageYOffset < 1620) {
+        if (window.pageYOffset > 1000 && window.pageYOffset < 1720) {
           app.classList.add("white");
           app.classList.remove("black");
         }
 
-        if (window.pageYOffset > 1620 && window.pageYOffset < 6740) {
+        if (window.pageYOffset > 1720 && window.pageYOffset < 6740) {
           app.classList.add("black");
           app.classList.remove("white");
           junc.classList.add("inactive");
@@ -645,15 +613,15 @@ window.addEventListener("load", () => {
     text.innerHTML = "Get in touch";
   });
 
-  white2.addEventListener("mouseenter", () => {
-    ctext.innerHTML = "";
-    ctext.appendChild(document.createElement("img")).src =
-      "./image/blackMail.png";
-  });
+  // white2.addEventListener("mouseenter", () => {
+  //   ctext.innerHTML = "";
+  //   ctext.appendChild(document.createElement("img")).src =
+  //     "./image/blackMail.png";
+  // });
 
-  white2.addEventListener("mouseleave", () => {
-    ctext.innerHTML = "Get in touch";
-  });
+  // white2.addEventListener("mouseleave", () => {
+  //   ctext.innerHTML = "Get in touch";
+  // });
 
   blackBtn.addEventListener("mouseenter", () => {
     btext.innerHTML = "";
@@ -664,34 +632,6 @@ window.addEventListener("load", () => {
   blackBtn.addEventListener("mouseleave", () => {
     btext.innerHTML = "Get in touch";
   });
-
-  // if (window.addEventListener)
-  //   window.addEventListener("DOMMouseScroll", wheel, false);
-  // window.onmousewheel = document.onmousewheel = wheel;
-
-  // function wheel(event) {
-  //   var delta = 0;
-  //   if (event.wheelDelta) delta = event.wheelDelta / 120;
-  //   else if (event.detail) delta = -event.detail / 3;
-
-  //   handle(delta);
-  //   if (event.preventDefault) event.preventDefault();
-  //   event.returnValue = false;
-  // }
-
-  // function handle(delta) {
-  //   var time = 1000;
-  //   var distance = 300;
-
-  //   $("html, body")
-  //     .stop()
-  //     .animate(
-  //       {
-  //         scrollTop: $(window).scrollTop() - distance * delta,
-  //       },
-  //       time
-  //     );
-  // }
 });
 
 digit.classList.remove("active");
@@ -716,7 +656,7 @@ function widthToOffset() {
     [901, 1100],
     [1100, 2600],
   ];
-  var offsets = [1720, 1870, 2255, 2475, 2135];
+  var offsets = [1720, 1870, 2055, 2475, 2135];
 
   windowSizes.forEach(function (eachSize, index) {
     if (isBetween(eachSize)) {
@@ -737,7 +677,7 @@ function shadeOffset() {
     [901, 1100],
     [1100, 2600],
   ];
-  var offsets = [1720, 1970, 2355, 2775, 2335];
+  var offsets = [1780, 1850, 2155, 2775, 2335];
 
   windowSizes.forEach(function (eachSize, index) {
     if (isBetween(eachSize)) {
